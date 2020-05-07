3fcefcc95888afc03d3094b796c7c4e4c169a163
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-07 22:07:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |cgl_keys, cgl_values|
     cgl_values.each do |keys, array_value|
       array_value.each do |names|
-        if pigeon_list[names] = nil
-        pigeon_list[names] = {}
+        if pigeon_list[names] == nil
+          pigeon_list[names] = {}
         end
         pigeon_list[names][cgl_keys] = []
         pigeon_list[names][cgl_keys] << keys.to_s
