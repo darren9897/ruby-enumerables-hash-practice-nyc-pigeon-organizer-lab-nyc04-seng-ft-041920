25db5066f1c4aa909e754eab174a6b4c6234ddb2
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-07 21:56:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,15 @@
-def
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |cgl_keys, cgl_values|
+    cgl_values.each do |keys, array_value|
+      array_value.each do |names|
+        pigeon_list[names] = {}
+        pigeon_list[names][]
+      end
+      
+    end
+    
+  end
 
 end
 
